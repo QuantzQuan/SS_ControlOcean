08289948af681801dfc1498010d667232ff20d15
Quantz <quanhongdagm@gmail.com>
2022-07-28 21:22:13 +0800

    Use on Raspberry to send Serial command to Arudino

---
+++ Zero_SS_Control.py
@@ -0,0 +1,38 @@
+import socket
+import time
+import serial
+
+class Client():
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def close(self):
+        self.client.close()
+
+
+if __name__ == "__main__":
+    # under windows system
+    ser = serial.Serial("com1", 115200, timeout=0.5)
+    # # under raspy
+    # ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.5)
+
+    # open com
+    if ser.isOpen():
+        ser.close()
+    ser.open()
+
+    # prepare socket
+    hostname = "192.168.3.18"
+    # hostname = "172.24.143.124" #发送端ip zerotier
+    port = 1212
+
+    while True:
+        client = Client()
+        try:
+            client.client.connect_ex((hostname, port))
+            cur_string = client.client.recv(1024)
+            print(client.client.recv(1024).decode())
+            ser.write(cur_string+b'\n')
+            client.close()
+        except:
+            time.sleep(1)
