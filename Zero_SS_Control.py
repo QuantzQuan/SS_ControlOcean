574d7ccf33b4d25d95b4a886b94f63ed41a6f22f
Quantz <quanhongdagm@gmail.com>
2022-08-07 11:30:42 +0800

    recieve command from PC and send to Arduino by Serial

--- Zero_SS_Control.py
+++ Zero_SS_Control.py
@@ -2,7 +2,10 @@ import socket
 import time
 import serial
 
-class Client():
+CONNECT_FLAG = 1
+
+
+class Client:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -12,27 +15,32 @@ class Client():
 
 if __name__ == "__main__":
     # under windows system
-    ser = serial.Serial("com1", 115200, timeout=0.5)
+    ser = serial.Serial("/dev/ttyUSB1", 115200, timeout=0.5)
     # # under raspy
     # ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.5)
 
-    # open com
+    # init to close serial
     if ser.isOpen():
         ser.close()
-    ser.open()
 
     # prepare socket
-    hostname = "192.168.76.27"    # sending device's IP
+    hostname = "192.168.50.137"  # sending device's IP
     # hostname = "172.24.143.124" # sending device's IP zerotier
     port = 1212
+    Raspi_Zero = Client()
+    while CONNECT_FLAG != 0:
+        try:
+            CONNECT_FLAG = Raspi_Zero.client.connect_ex((hostname, port))
+        except:
+            pass
 
     while True:
-        client = Client()
-        try:
-            client.client.connect_ex((hostname, port))
-            cur_string = client.client.recv(1024)
-            print(client.client.recv(1024).decode())
-            ser.write(cur_string+b'\n')
-            client.close()
-        except:
-            time.sleep(1)
+        ser.open()
+        cur_string = Raspi_Zero.client.recv(1024)
+        print(cur_string)
+        ser.write(cur_string + b'\n')
+        ser.flushOutput()
+        ser.close()
+        time.sleep(0.1)
+        if KeyboardInterrupt:
+            Raspi_Zero.close()
